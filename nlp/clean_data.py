5a79949037faaaabe806ad4ac5ea59a849aa21a8
erikqtrs <erik.anndy@gmail.com>
2022-04-02 18:21:24 -0500

    commit

--- nlp/clean_data.py
+++ nlp/clean_data.py
@@ -1,12 +1,10 @@
 import re
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
 def clean_text(text):
     """
         Function to clean each tweet text
     """
-    stop_words = list( stopwords.words('english') )
     text = re.sub(r'^RT[\s]+', '', text)
     text = re.sub(r'https?:\/\/.*[\r\n]*', '', text)
     text = re.sub(r'#', '', text)
@@ -16,10 +14,8 @@ def clean_text(text):
     text = re.sub(r':+', '', text)
     text = text.lower()
     text_token = word_tokenize(text)
-    clean_words = [w for w in text_token if w not in stop_words]
-
-    # remove punctuation
-    unpuctuated_text = [char for char in clean_words if char not in string.punctuation]
+        # remove punctuation
+    unpuctuated_text = [char for char in text_token if char not in string.punctuation]
     clean_text = ' '.join(unpuctuated_text)
     
     return clean_text
